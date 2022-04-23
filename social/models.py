42b1a8aabe6fe17ec30cbeb0114f8637c2ed76e6
pedalopon <pedropontiga@gmail.com>
2022-04-23 20:34:24 +0200

    #13-feat: Added User, Post and Notification model

--- social/models.py
+++ social/models.py
@@ -1,11 +1,25 @@
+from datetime import datetime
+
 from mongoengine import Document, fields
 
 
-class Meta:
-    app_label = 'social'
+class User(Document):
+    username = fields.StringField(max_length=30, required=True)
+    email = fields.EmailField(required=True)
+    password = fields.StringField()
+    birthDate = fields.DateTimeField()
+    first_name = fields.StringField(max_length=30, required=True)
+    lastName = fields.StringField(max_length=60, required=True)
 
 
 class Post(Document):
-    tittle = fields.StringField(max_length=30)
-    content = fields.StringField(max_length=30)
-    creation_date = fields.DateTimeField()
+    title = fields.StringField(max_length=30, required=True)
+    content = fields.StringField(max_length=1250)
+    creation_date = fields.DateTimeField(default=datetime.utcnow)
+
+
+class Notification(Document):
+    title = fields.StringField(max_length=30, required=True)
+    content = fields.StringField(max_length=1250)
+    creation_date = fields.DateTimeField(default=datetime.utcnow)
+    user = fields.ReferenceField(User)
