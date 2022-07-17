6a6c16b016d165526560c36fc6991ec96a10f0c6
pedalopon <pedropontiga@gmail.com>
2022-07-17 21:11:23 +0200

    #35-feat: Activada comprobacion de estar logeado en todos los endpoints

--- doit/settings.py
+++ doit/settings.py
@@ -95,6 +95,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
