d5e5290e9203ffb61a71041049430c9958f634ec
Jose Nieves <esoj.nieves@gmail.com>
2022-07-23 12:56:37 +0200

    #40-fix: get permissions

--- auth/permissions.py
+++ auth/permissions.py
@@ -10,10 +10,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Objects that has an owner can't be modified by other users
         mongo_user = User.objects.get(user_id=request.user.id)
-        if hasattr(obj, 'createdBy'):
-            return mongo_user == obj.createdBy
-        elif hasattr(obj, 'goal') and obj.goal:
-            goal = Goal.objects.get(id=obj.goal.id)
-            return goal and goal.createdBy == mongo_user
-        else:
+        # if the request is not a get request, the user must be the owner
+
+        if request.method in permissions.SAFE_METHODS:
             return True
+        else:
+            if hasattr(obj, 'createdBy'):
+                return mongo_user == obj.createdBy
+            elif hasattr(obj, 'goal') and obj.goal:
+                goal = Goal.objects.get(id=obj.goal.id)
+                return goal and goal.createdBy == mongo_user
+            else:
+                return True
